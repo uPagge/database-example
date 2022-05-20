bab20a3d48f6f6788670bb903dd7465844a22196
Struchkov Mark <mark@struchkov.dev>
2022-05-20 20:02:34 +0300

    InitCommit

---
+++ src/main/java/dev/struchkov/example/database/ExampleDatabaseApplication.java
@@ -0,0 +1,13 @@
+package dev.struchkov.example.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/struchkov/example/database/ExampleDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.struchkov.example.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
